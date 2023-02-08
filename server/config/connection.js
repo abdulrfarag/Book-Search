a81344f2035d6a1518bc727a4671023381d44dcc
abdulrfarag <ar060290@gmail.com>
2023-02-08 09:52:41 -0600

    updated connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,27 +1,11 @@
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
-
-// module.exports = mongoose.connection;
-
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-// );
-
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 module.exports = mongoose.connection;
