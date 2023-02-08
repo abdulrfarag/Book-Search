f8b8bc47cd659bacae314908b05f16279b921fa6
abdulrfarag <ar060290@gmail.com>
2023-02-08 02:03:57 -0600

    added comments to my resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,13 +4,14 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    // By adding context to our query, we can retrieve the logged in user without specifically searching for them
     me: async (parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
 
         return userData;
       }
-
+    
       throw new AuthenticationError('Not logged in');
     },
   },
@@ -38,6 +39,8 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+
+    // Add a third argument to the resolver to access data in our `context`
     saveBook: async (parent, { bookData }, context) => {
       if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
@@ -49,8 +52,10 @@ const resolvers = {
         return updatedUser;
       }
 
+      // If user attempts to execute this mutation and isn't logged in, throw an error
       throw new AuthenticationError('You need to be logged in!');
     },
+        // Make it so a logged in user can only remove a book from their own profile
     removeBook: async (parent, { bookId }, context) => {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
@@ -62,6 +67,7 @@ const resolvers = {
         return updatedUser;
       }
 
+      // If user attempts to execute this mutation and isn't logged in, throw an error
       throw new AuthenticationError('You need to be logged in!');
     },
   },
